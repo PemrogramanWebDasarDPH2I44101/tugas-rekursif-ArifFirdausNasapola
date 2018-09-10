124357b23df8a32e6562f58744b8e2ce4a2030b7
ArifFirdausNasapola <43140658+ArifFirdausNasapola@users.noreply.github.com>
2018-09-10 20:31:12 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -1,10 +1,31 @@
-<?php
+<form action=" " method="GET">
+	masukkan angka: <input type="text" name="nilai">
+	<input type="submit" name="submit" value="tampil">
+	</form>
 
-function rekursif($i){
-  echo "$i <br>";
-  $i--;
-  if($i>0)
-    rekursif($i);
-}
-rekursif(5);
+	<?php
+	if (isset($_GET['submit'])) {
+	
+	
+function segitiga($baris, $col, $batas){
+		$batas= $_GET['nilai'];
+
+	if ($baris = $batas){
+
+		if($col = $baris){
+			echo("*");
+		}
+
+		if($col = $baris){
+			echo ("<br>");
+			segitiga(--$baris, 1, $batas);
+		}else{
+			segitiga($baris, --$col, $batas);
+
+			}
+		}
+	}
+
+		segitiga(1,1,10);
+	}
 ?>
