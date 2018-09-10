fa0f9f9a9fae1ec10b5c7e52d486f2742ae39184
ArifFirdausNasapola <43140658+ArifFirdausNasapola@users.noreply.github.com>
2018-09-10 20:32:32 +0700

    Update rekursif.php

--- rekursif.php
+++ rekursif.php
@@ -5,8 +5,7 @@
 
 	<?php
 	if (isset($_GET['submit'])) {
-	
-	
+echo "<br>";	
 function segitiga($baris, $col, $batas){
 		$batas= $_GET['nilai'];
 
@@ -15,17 +14,14 @@ function segitiga($baris, $col, $batas){
 		if($col = $baris){
 			echo("*");
 		}
-
 		if($col = $baris){
 			echo ("<br>");
 			segitiga(--$baris, 1, $batas);
 		}else{
 			segitiga($baris, --$col, $batas);
-
-			}
 		}
 	}
-
+}
 		segitiga(1,1,10);
 	}
 ?>
